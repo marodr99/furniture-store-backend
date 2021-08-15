e96cef4a7738ef481dd98f9b7d63cb864f8829df
Marek Odrzywolek <marcos8679@gmail.com>
2021-08-15 22:46:34 +0200

    Initial commit

---
+++ src/main/java/pk/furniturestorebackend/FurnitureStoreBackendApplication.java
@@ -0,0 +1,13 @@
+package pk.furniturestorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurnitureStoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurnitureStoreBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pk/furniturestorebackend/FurnitureStoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pk.furniturestorebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurnitureStoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
